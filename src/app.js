4efea9137d80ca35f1970478534fc66c830ba571
Unknown <kjhop@wp.pl>
2018-07-24 15:19:31 +0200

    Setup of app and todoController

--- src/app.js
+++ src/app.js
@@ -1,5 +1,13 @@
-const app = require('express');
+const express = require('express');
+const todoController = require('./controllers/todoController');
 
-
-
+const app = express();
+//Set template engine
+app.set('view engine', 'ejs');
+//Static files
+app.use('/assets', express.static('../public'));
+//Start controllers
+todoController(app);
+//Listen to port
 app.listen(8080);
+console.log('Server started. Now listening...');
---
+++ src/controllers/todoController.js
@@ -0,0 +1,11 @@
+module.exports = app =>{
+    app.get('/todo', (request, response)=>{
+
+    });
+    app.post('/todo', (request, response)=>{
+        
+    });
+    app.delete('/todo', (request, response)=>{
+        
+    });
+};
